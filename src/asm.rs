rust-embedded/riscv-rt

commit 9a02223ac3b67d90c3544a343cf07b70df48c5e4

Add feature single-hart
Signed-off-by: Chien Wong <m@xv97.com>

src/asm.rs
@@ -86,21 +86,21 @@ _abs_start:
     .option norelax
     la gp, __global_pointer$
     .option pop",
-    #[cfg(feature = "s-mode")]
+    #[cfg(all(not(feature = "single-hart"), feature = "s-mode"))]
     "mv t2, a0 // the hartid is passed as parameter by SMODE",
-    #[cfg(not(feature = "s-mode"))]
+    #[cfg(all(not(feature = "single-hart"), not(feature = "s-mode")))]
     "csrr t2, mhartid",
+    #[cfg(not(feature = "single-hart"))]
     "lui t0, %hi(_max_hart_id)
     add t0, t0, %lo(_max_hart_id)
-    bgtu t2, t0, abort
-
-    // Allocate stacks
+    bgtu t2, t0, abort",
+    "// Allocate stacks
     la sp, _stack_start
     lui t0, %hi(_hart_stack_size)
     add t0, t0, %lo(_hart_stack_size)",
-    #[cfg(riscvm)]
+    #[cfg(all(not(feature = "single-hart"), riscvm))]
     "mul t0, t2, t0",
-    #[cfg(not(riscvm))]
+    #[cfg(all(not(feature = "single-hart"), not(riscvm)))]
     "beqz t2, 2f  // Jump if single-hart
     mv t1, t2
     mv t3, t0
@@ -110,8 +110,8 @@ _abs_start:
     bnez t1, 1b
 2:  ",
     "sub sp, sp, t0
-    
-    // Set frame pointer 
+
+    // Set frame pointer
     add s0, sp, zero
 
     jal zero, _start_rust
@@ -121,9 +121,9 @@ _abs_start:
 
 /// Trap entry point (_start_trap). It saves caller saved registers, calls
 /// _start_trap_rust, restores caller saved registers and then returns.
-/// 
+///
 /// # Usage
-/// 
+///
 /// The macro takes 5 arguments:
 /// - `$STORE`: the instruction used to store a register in the stack (e.g. `sd` for riscv64)
 /// - `$LOAD`: the instruction used to load a register from the stack (e.g. `ld` for riscv64)

Cargo.toml
@@ -12,6 +12,7 @@ edition = "2018"
 
 [features]
 s-mode = []
+single-hart = []
 
 [dependencies]
 r0 = "1.0.0"

src/lib.rs
@@ -325,6 +325,10 @@
 //!
 //! # Features
 //!
+//! ## `single-hart`
+//!
+//! This feature saves a little code size by removing unnecessary stack space calculation if there is only one hart on the target.
+//!
 //! ## `s-mode`
 //!
 //! The supervisor mode feature (`s-mode`) can be activated via [Cargo features](https://doc.rust-lang.org/cargo/reference/features.html).

commit 93f2bba6e2373a551649db8979c1cf7b6adb5134

Fix setting incorrect sp if single-hart
If single-hart is enabled, the sp is set to _stack_start - _hart_stack_size,
rather than _stack_start. Fix this.

Fixes: 9a02223ac3b6 ("Add feature single-hart")
Signed-off-by: Chien Wong <m@xv97.com>

src/asm.rs
@@ -85,7 +85,8 @@ _abs_start:
     .option push
     .option norelax
     la gp, __global_pointer$
-    .option pop",
+    .option pop
+    // Allocate stacks",
     #[cfg(all(not(feature = "single-hart"), feature = "s-mode"))]
     "mv t2, a0 // the hartid is passed as parameter by SMODE",
     #[cfg(all(not(feature = "single-hart"), not(feature = "s-mode")))]
@@ -93,9 +94,7 @@ _abs_start:
     #[cfg(not(feature = "single-hart"))]
     "lui t0, %hi(_max_hart_id)
     add t0, t0, %lo(_max_hart_id)
-    bgtu t2, t0, abort",
-    "// Allocate stacks
-    la sp, _stack_start
+    bgtu t2, t0, abort
     lui t0, %hi(_hart_stack_size)
     add t0, t0, %lo(_hart_stack_size)",
     #[cfg(all(not(feature = "single-hart"), riscvm))]
@@ -109,9 +108,10 @@ _abs_start:
     addi t1, t1, -1
     bnez t1, 1b
 2:  ",
-    "sub sp, sp, t0
-
-    // Set frame pointer
+    "la sp, _stack_start",
+    #[cfg(not(feature = "single-hart"))]
+    "sub sp, sp, t0",
+    "// Set frame pointer
     add s0, sp, zero
 
     jal zero, _start_rust